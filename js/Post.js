ad6513cf8077ae16888186feca1e90c193d96a45
Simon Radosavljević <simon.fhz@gmail.com>
2025-01-18 18:11:41 +0100

    Finally all fixed

--- js/Post.js
+++ js/Post.js
@@ -1,62 +1,74 @@
-class Post{
-	post_id="";
-	post_content="";
-	user_id="";
-	likes="";
-	api_url='https://63ad8e14ceaabafcf165b15d.mockapi.io';
+class Post {
+    post_id = "";
+    post_content = "";
+    user_id = "";
+    likes = 0;
+    likedBy = [];
+    api_url = 'https://63ad8e14ceaabafcf165b15d.mockapi.io';
 
-	async create(){
-		let session=new Session();
-		sesion_id=session.getSession();
-		let data={
-			user_id: sesion_id,
-			content: this.post_content,
-			likes:0
-		}
+    async create() {
+        let session = new Session();
+        let session_id = session.getSession();
+        let data = {
+            user_id: session_id,
+            content: this.post_content,
+            likes: 0,
+            likedBy: [],
+        };
 
-		data=JSON.stringify(data);
+        data = JSON.stringify(data);
 
-		let response= await fetch(this.api_url+"/posts",{
-			method: 'POST',
-			headers:{
-				'Content-Type': 'application/json'
-			},
-			body:data
-		});
-		data= await response.json();
+        let response = await fetch(this.api_url + "/posts", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: data,
+        });
+        data = await response.json();
 
-		return data;
-	}
-	async getAllPosts(){
-		let response = await fetch(this.api_url+'/posts');
-		let data = await response.json();
-		return data;
-	}
-	like(post_id,likes){
-		let data={
-			likes:likes,
-		};
+        return data;
+    }
 
-		data=JSON.stringify(data);
-		fetch(this.api_url+'/posts/'+post_id,{
-			method: 'PUT',
-			headers:{
-				'Content-Type': 'application/json',
-			},
-			body: data
-		})
-		.then(response=>response.json())
-		.then(data=>{alert("post lajkovan")})
+    async getAllPosts() {
+        let response = await fetch(this.api_url + '/posts');
+        let data = await response.json();
+        return data;
+    }
 
-	}
+    async like(post_id, user_id) {
+        let response = await fetch(this.api_url + '/posts/' + post_id);
+        let post = await response.json();
 
+        if (!post.likedBy.includes(user_id)) {
+            post.likes++;
+            post.likedBy.push(user_id);
 
-	delete(post_id){
-		fetch(this.api_url+'/posts/'+post_id,{
-			method: 'DELETE'
-		})
-		.then(response=>response.json())
-		.then(data=>{alert("Post je obrisan")});
+            let data = JSON.stringify({
+                likes: post.likes,
+                likedBy: post.likedBy,
+            });
 
-	}
+            await fetch(this.api_url + '/posts/' + post_id, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: data,
+            });
+            alert("Post lajkovan");
+        } else {
+            alert("Već ste lajkovali ovaj post!");
+        }
+    }
+
+    delete(post_id) {
+        fetch(this.api_url + '/posts/' + post_id, {
+            method: 'DELETE',
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert("Post je obrisan");
+            });
+    }
 }
--- js/hexa.js
+++ js/hexa.js
@@ -1,236 +1,212 @@
-let sesion=new Session();
+let sesion = new Session();
 sesion_id = sesion.getSession();
-let komentar=true;
-let dozvola=true;
+let komentar = true;
 
-if(sesion_id!==""){
-	async function populateUserData(){
-		let user=new User();
-		user=await user.get(sesion_id);
-	document.querySelector("#username").innerText=user['username'];
-	document.querySelector("#email").innerText=user['email'];
+if (sesion_id !== "") {
+    async function populateUserData() {
+        let user = new User();
+        user = await user.get(sesion_id);
+        document.querySelector("#username").innerText = user['username'];
+        document.querySelector("#email").innerText = user['email'];
 
-	document.querySelector("#korisnicko_ime").value=user['username'];
-	document.querySelector("#edit_email").value=user['email'];
-	}
+        document.querySelector("#korisnicko_ime").value = user['username'];
+        document.querySelector("#edit_email").value = user['email'];
+    }
 
-	populateUserData();
-}
-else
-{
-	window.location.href="/";
+    populateUserData();
+} else {
+    window.location.href = "/";
 }
 
+document.querySelector("#logout").addEventListener("click", e => {
+    e.preventDefault();
 
-document.querySelector("#logout").addEventListener("click",e=>{
-	e.preventDefault();
-
-	sesion.destroySession();
-	window.location.href="/";
-})
-
-document.querySelector("#editAccount").addEventListener("click",()=>{
-	document.querySelector(".custom-modal").style.display="block";
-})
-document.querySelector("#closeModal").addEventListener("click",()=>{
-	document.querySelector(".custom-modal").style.display="none";
-})
-
-document.querySelector("#editForm").addEventListener("submit",e=>{
-	e.preventDefault();
-	let user=new User();
-	user.username=document.querySelector("#korisnicko_ime").value;
-	user.email=document.querySelector("#edit_email").value;
-	user.edit();
-})
-
-document.querySelector("#deleteProfile").addEventListener("click",e=>{
-	e.preventDefault();
-
-	let text="Da li ste sigurni da zelite da obrisete profil?";
-
-	if(confirm(text)===true){
-		let user = new User();
-		user.delete();
-	}
-})
-
-document.querySelector("#postForm").addEventListener("submit",e=>{
-	e.preventDefault();
-
-	async function createPost(){
-		let content = document.querySelector("#postContent").value;
-		document.querySelector("#postContent").value="";
-		let post= new Post();
-		post.post_content=content;
-		post =await post.create();
-
-		let current_user= new User();
-		current_user=await current_user.get(sesion_id);
-
-		let html= document.querySelector("#allPostsWrapper").innerHTML;
-
-		let delete_post_html='';
-		if(sesion_id===post.user_id){
-			delete_post_html=`<button class="remove-btn" onclick="removeMyPost(this)"></button>`;
-		}
-
-		document.querySelector("#allPostsWrapper").innerHTML=`<div class="single-post" data_post_id="${post.id}">
-		<div class="post-actions">
-																			<p><b>${current_user.username}</b></p>
-																		   <div>
-																		<div class="post-content"><p class="postTekst">${post.content}</p></div> 
-
-
-																		
-																		   		<button onclick="likePost(this)" class="likePostJS like-btn"><span>${post.likes}</span></button>
-																		   		<button class="comment-btn" onclick="commentPost(this)">0</button>
-																		   		${delete_post_html}
-																		   </div>
-																		 </div>
-
-
-																		<div class="post-comments">
-																<form>
-																	<input placeholder="Napiši komentar..." type="text">
-																	<button class="comment" onlick="commentPostSubmit(event)">Comment</button>
-																</form>
-
-																			
-																		 
-																
-																		</div>
-															</div>
-															
-
-
-															`+html;
-
-	}
-	createPost();
+    sesion.destroySession();
+    window.location.href = "/";
 });
 
-async function getAllPosts(){
-	let all_posts=new Post();
-	all_posts=await all_posts.getAllPosts();
+document.querySelector("#editAccount").addEventListener("click", () => {
+    document.querySelector(".custom-modal").style.display = "block";
+});
+document.querySelector("#closeModal").addEventListener("click", () => {
+    document.querySelector(".custom-modal").style.display = "none";
+});
 
-	
-	all_posts.forEach(post=>{
-		async function getPostUser(){
-			let user=new User();
-			user=await user.get(post.user_id);
-		
-			let comments=new Comment();
-			comments=await comments.get(post.id);
-			let comments_html='';
-			if(comments.length>0){
-				comments.forEach(comment=>{
-					comments_html+=`<div class="single-comment">${comment.content}</div>`
-				});
-			}
-			console.log(comments);
+document.querySelector("#editForm").addEventListener("submit", e => {
+    e.preventDefault();
+    let user = new User();
+    user.username = document.querySelector("#korisnicko_ime").value;
+    user.email = document.querySelector("#edit_email").value;
+    user.edit();
+});
 
-		let delete_post_html="";
-		let html=document.querySelector("#allPostsWrapper").innerHTML;
-		if(sesion_id===post.user_id){
-			 delete_post_html=`<button class="remove-btn" onclick="removeMyPost(this)"></button>`;
-		}
-		document.querySelector("#allPostsWrapper").innerHTML=`<div class="single-post" data_post_id="${post.id}">
-																		<div class="post-actions">
-																			<p><b>${user.username}</b></p>
-																		   <div>
-																		<div class="post-content"><p class="postTekst">${post.content}</p></div> 
+document.querySelector("#deleteProfile").addEventListener("click", e => {
+    e.preventDefault();
 
+    let text = "Da li ste sigurni da zelite da obrisete profil?";
 
-																		
-																		   		<button onclick="likePost(this)" class="likePostJS like-btn"><span>${post.likes}</span></button>
-																		   		<button class="comment-btn" onclick="commentPost(this)">0</button>
-																		   		${delete_post_html}
-																		   </div>
-																		 </div>
+    if (confirm(text) === true) {
+        let user = new User();
+        user.delete();
+    }
+});
 
+document.querySelector("#postForm").addEventListener("submit", e => {
+    e.preventDefault();
 
-																		<div class="post-comments">
-																<form>
-																	<input placeholder="Napiši komentar..." type="text">
-																	<button class="comment" onclick="commentPostSubmit(event)">Komentariši</button>
-																</form>
-																
-																			
-																		   
-																${comments_html}
-																		</div>
-															</div>
-															
+    async function createPost() {
+        let content = document.querySelector("#postContent").value;
+        document.querySelector("#postContent").value = "";
+        let post = new Post();
+        post.post_content = content;
+        post = await post.create();
 
+        let current_user = new User();
+        current_user = await current_user.get(sesion_id);
 
-															` +html;
-		}
-		getPostUser();
-	});
+        let html = document.querySelector("#allPostsWrapper").innerHTML;
+
+        let delete_post_html = '';
+        if (sesion_id === post.user_id) {
+            delete_post_html = `<button class="remove-btn" onclick="removeMyPost(this)"></button>`;
+        }
+
+        document.querySelector("#allPostsWrapper").innerHTML = `<div class="single-post" data_post_id="${post.id}">
+            <div class="post-actions">
+                <p><b>${current_user.username}</b></p>
+                <div>
+                    <div class="post-content"><p class="postTekst">${post.content}</p></div> 
+                    <button class="likePostJS like-btn"><span>${post.likes}</span></button>
+                    <button class="comment-btn" onclick="commentPost(this)">${0}</button>
+                    ${delete_post_html}
+                </div>
+            </div>
+            <div class="post-comments">
+                <form>
+                    <input placeholder="Napiši komentar..." type="text">
+                    <button class="comment" onclick="commentPostSubmit(event)">Comment</button>
+                </form>
+            </div>
+        </div>` + html;
+
+        // Dodavanje event listener-a za novo dugme like
+        document.querySelectorAll('.likePostJS').forEach(btn => {
+            btn.addEventListener("click", function() {
+                likePost(this);
+            });
+        });
+    }
+
+    createPost();
+});
+
+async function getAllPosts() {
+    let all_posts = new Post();
+    all_posts = await all_posts.getAllPosts();
+
+    all_posts.forEach(post => {
+        async function getPostUser() {
+            let user = new User();
+            user = await user.get(post.user_id);
+
+            let comments = new Comment();
+            comments = await comments.get(post.id);
+            let comments_html = '';
+            if (comments.length > 0) {
+                comments.forEach(comment => {
+                    comments_html += `<div class="single-comment">${comment.content}</div>`;
+                });
+            }
+
+            let delete_post_html = "";
+            let html = document.querySelector("#allPostsWrapper").innerHTML;
+            if (sesion_id === post.user_id) {
+                delete_post_html = `<button class="remove-btn" onclick="removeMyPost(this)"></button>`;
+            }
+            document.querySelector("#allPostsWrapper").innerHTML = `<div class="single-post" data_post_id="${post.id}">
+                <div class="post-actions">
+                    <p><b>${user.username}</b></p>
+                    <div>
+                        <div class="post-content"><p class="postTekst">${post.content}</p></div> 
+                        <button class="likePostJS like-btn"><span>${post.likes}</span></button>
+                        <button class="comment-btn" onclick="commentPost(this)">${comments.length}</button>
+                        ${delete_post_html}
+                    </div>
+                </div>
+                <div class="post-comments">
+                    <form>
+                        <input placeholder="Napiši komentar..." type="text">
+                        <button class="comment" onclick="commentPostSubmit(event)">Komentariši</button>
+                    </form>
+                    ${comments_html}
+                </div>
+            </div>` + html;
+
+            // Dodavanje event listener-a za novo dugme like
+            document.querySelectorAll('.likePostJS').forEach(btn => {
+                btn.addEventListener("click", function() {
+                    likePost(this);
+                });
+            });
+        }
+
+        getPostUser();
+    });
 }
 
 getAllPosts();
 
-const commentPostSubmit =e=>{
-	e.preventDefault();
-	let btn=e.target;
-	btn.setAttribute("disabled",'true');
+const commentPostSubmit = e => {
+    e.preventDefault();
+    let btn = e.target;
+    btn.setAttribute("disabled", 'true');
 
-	let main_post_el=btn.closest(".single-post");
-	let post_id=main_post_el.getAttribute("data_post_id");
+    let main_post_el = btn.closest(".single-post");
+    let post_id = main_post_el.getAttribute("data_post_id");
 
-	let comment_value=main_post_el.querySelector("input").value;
+    let comment_value = main_post_el.querySelector("input").value;
 
-	main_post_el.querySelector("input").value=" ";
+    main_post_el.querySelector("input").value = "";
 
-	main_post_el.querySelector(".post-comments").innerHTML+=`<div class="single-comment">${comment_value}</div>`;
+    main_post_el.querySelector(".post-comments").innerHTML += `<div class="single-comment">${comment_value}</div>`;
 
-	let comment=new Comment();
-	comment.content=comment_value;
-	comment.user_id=sesion_id;
-	comment.post_id=post_id;
-	comment.create();
-}
+    let comment = new Comment();
+    comment.content = comment_value;
+    comment.user_id = sesion_id;
+    comment.post_id = post_id;
+    comment.create();
+};
 
-const removeMyPost = btn =>{
-	let post_id= btn.closest(".single-post").getAttribute("data_post_id");
+const removeMyPost = btn => {
+    let post_id = btn.closest(".single-post").getAttribute("data_post_id");
 
-	btn.closest(".single-post").remove();
+    btn.closest(".single-post").remove();
 
+    let post = new Post();
+    post.delete(post_id);
+};
 
-	let post=new Post();
-	post.delete(post_id);
-}
+const likePost = async btn => {
+    let main_post_el = btn.closest(".single-post");
+    let post_id = main_post_el.getAttribute("data_post_id");
+    let number_of_likes = parseInt(btn.querySelector("span").innerText);
 
-const likePost = btn=>{
-	let main_post_el=btn.closest(".single-post");
-	let post_id= btn.closest(".single-post").getAttribute("data_post_id");
-	let number_of_likes=parseInt(btn.querySelector("span").innerText);
-	
-	
-	btn.querySelector("span").innerText=number_of_likes+1;
-	
-	btn.setAttribute("disabled",'true');
+    let post = new Post();
+    await post.like(post_id, sesion_id);
 
-	let post=new Post();
-	post.like(post_id,number_of_likes+1);
-	
-}
+    // Osvežavanje broja lajkova nakon što je uspešno lajkovano
+    btn.querySelector("span").innerText = number_of_likes + 1;
+    btn.setAttribute("disabled", 'true');
+};
 
-const commentPost=btn=>{
-
-	let main_post_el = btn.closest('.single-post');
-	let post_id=main_post_el.getAttribute("data-post_id");
-	if(komentar===true)
-	{
-		main_post_el.querySelector(".post-comments").style.display="block"; 
-		komentar=false;
-	}
-	else if(komentar===false)
-	{
-		main_post_el.querySelector(".post-comments").style.display="none"; 
-		komentar=true;
-	}
-
-}
+const commentPost = btn => {
+    let main_post_el = btn.closest('.single-post');
+    let post_id = main_post_el.getAttribute("data_post_id");
+    if (komentar === true) {
+        main_post_el.querySelector(".post-comments").style.display = "block"; 
+        komentar = false;
+    } else if (komentar === false) {
+        main_post_el.querySelector(".post-comments").style.display = "none"; 
+        komentar = true;
+    }
+};
